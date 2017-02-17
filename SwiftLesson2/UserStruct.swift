7dead75f4111a5d1110155100c909890d893e583
Leklef <lenar8553@gmail.com>
2017-02-17 17:53:51 +0300

    Поправил валидацию пароля

--- SwiftLesson2/UserStruct.swift
+++ SwiftLesson2/UserStruct.swift
@@ -17,7 +17,7 @@ extension User{
     var isValidUserData: Bool {
         let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
-        let passwordFormat:String = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$"
+        let passwordFormat:String = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$"
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordFormat)
         let correct:Bool =  emailPredicate.evaluate(with: self.email)&&passwordPredicate.evaluate(with: self.password)
         
